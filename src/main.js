be7cb0a3af6941dc724ba09cd32f0b313c53983d
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-23 09:50:22 -0700

    add css style

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from '../src/js/exchange-service.js'
+
+
 
 
